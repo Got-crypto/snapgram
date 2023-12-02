e4b0956e574f3b0d24c19214d72d5f080c5ce144
got-crypto <pecpalmplay@gmail.com>
2023-12-02 23:24:37 +0200

    modified tailwindcss config

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
-import './global.css'
+import './globals.css'
 
 export default function App() {
   return (
-    <div>App</div>
+    <h1 className="text-3xl font-bold underline">
+        Hello Snapgram!
+    </h1>
   )
 }
