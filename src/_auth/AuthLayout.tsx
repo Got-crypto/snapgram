ed465934a77d9ec399570c4b05bab12578f69781
got-crypto <pecpalmplay@gmail.com>
2023-12-03 20:13:27 +0200

    added assets to public folder

--- src/_auth/AuthLayout.tsx
+++ src/_auth/AuthLayout.tsx
@@ -8,9 +8,11 @@ export default function AuthLayout() {
       {isAuthenticated ? (
         <Navigate to="/" />
       ) : (
-        <section>
-          <Outlet />
-        </section>
+        <>
+          <section className="flex flex-1 justify-center items-center flex-col py-10">
+            <Outlet />
+          </section>
+        </>
       )}
     </>
   )
